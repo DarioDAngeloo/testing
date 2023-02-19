1fd6d59829e2e4f038bef467922d5ff094747fb2
Dario DAngelo <dariodangelo21@gmail.com>
2023-02-19 11:22:15 -0300

    2

---
+++ app/src/androidTest/java/com/study/testingbeforetechnicalinterview/ResourceComparerTest.kt
@@ -0,0 +1,25 @@
+package com.study.testingbeforetechnicalinterview
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ResourceComparerTest {
+    private val resourceComparer = ResourceComparer()
+
+    @Test
+    fun ifTheResID_isEqual_to_String__assertTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context,R.string.app_name,"TestingBeforeTechnicalInterview")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun ifTheResID_isDifferent_to_String__assertFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context,R.string.app_name,"Hola")
+        assertThat(result).isFalse()
+    }
+
+}
